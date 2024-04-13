f7e26afdaa4b540a891b785944114a9e6cc99920
Oyvind Tafjord <OyvindTafjord@users.noreply.github.com>
2024-04-13 11:05:42 -0700

    Change package name to avoid install conflict

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lm_eval",
+    name="lm_eval_custom",
     version="0.3.0",
     author="Leo Gao",
     author_email="lg@eleuther.ai",
